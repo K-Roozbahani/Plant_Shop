2ab93952808a1f3ac09d60f74045fe1f1dd5493f
k-roozbahani <kavehrozbah@gmail.com>
2023-01-22 17:07:02 +0330

    created user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    message = "Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+
+    phone_number = models.CharField(verbose_name=_('phone number'), max_length=16, unique=True,
+                                    validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message=message)])
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
