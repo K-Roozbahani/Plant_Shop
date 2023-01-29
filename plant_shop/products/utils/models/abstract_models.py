b4d385261241fd65f47bc70eb9ea05cf734028b7
k-roozbahani <kavehrozbah@gmail.com>
2023-01-29 16:47:17 +0330

    add ValidManeger to abstracts models

--- plant_shop/products/utils/models/abstract_models.py
+++ plant_shop/products/utils/models/abstract_models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class ValidManager(models.Manager):
+    def get_queryset(self):
+        return super(ValidManager, self).get_queryset().filter(is_valid=True)
+
+
 class AbstractModel(models.Model):
     is_valid = models.BooleanField(verbose_name=_('is valid'), default=True)
     created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(verbose_name=_('updated time'), auto_now=True)
+
+    objects = models.Manager()
+    valid_objects = ValidManager()
 
     class Meta:
         abstract = True
