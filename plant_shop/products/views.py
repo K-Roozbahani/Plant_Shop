e9725f9ac4169e8e8c03600229d979aceae7cb8d
k-roozbahani <kavehrozbah@gmail.com>
2023-02-03 18:10:48 +0330

    add products list view

--- plant_shop/products/views.py
+++ plant_shop/products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .api.views import ProductApiView
 
-# Create your views here.
+
+def product_view(request, pk=None):
+    if not pk:
+        api_response = ProductApiView.as_view({'get': 'list'})(request).data
+        print(api_response)
+        products = [dict(product) for product in api_response]
+        print(products)
+        context = {'products': products}
+        return render(request, 'products.html', context)
+    elif pk:
+        if not pk:
+            api_response = ProductApiView.as_view({'get': 'retrieve'})(request).data
+            print(api_response)
+            productc = dict(api_response)
+            print(productc)
+            context = {'products': productc}
+            return render(request, 'products.html', context)
