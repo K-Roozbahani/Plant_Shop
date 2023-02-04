574fc7740bfa40ef5834a690da62847adf0f954b
k-roozbahani <kavehrozbah@gmail.com>
2023-02-04 23:10:11 +0330

    remove prints into product view

--- plant_shop/products/views.py
+++ plant_shop/products/views.py
@@ -5,16 +5,12 @@ from .api.views import ProductApiView
 def product_view(request, pk=None):
     if not pk:
         api_response = ProductApiView.as_view({'get': 'list'})(request).data
-        print(api_response)
         products = [dict(product) for product in api_response]
-        print(products)
         context = {'products': products}
         return render(request, 'products.html', context)
     elif pk:
         if not pk:
             api_response = ProductApiView.as_view({'get': 'retrieve'})(request).data
-            print(api_response)
-            productc = dict(api_response)
-            print(productc)
-            context = {'products': productc}
+            product = dict(api_response)
+            context = {'products': product}
             return render(request, 'products.html', context)
