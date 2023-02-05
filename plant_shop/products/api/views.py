4c1cf9fe1d42cb9a755dc0a0f25b908b60e6a9d2
k-roozbahani <kavehrozbah@gmail.com>
2023-02-05 18:10:03 +0330

    add api view categoy model

--- plant_shop/products/api/views.py
+++ plant_shop/products/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 from rest_framework import mixins
-from ..models import Product
-from .serializers import ProductSerializer
+from ..models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 
 class ProductApiView(viewsets.ReadOnlyModelViewSet):
     queryset = Product.valid_objects.all()
     serializer_class = ProductSerializer
+
+
+class CategoryApiView(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.valid_objects.perifetch_related('sub_categories').all()
+    serializer_class = CategorySerializer
