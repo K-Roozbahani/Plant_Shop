0799a795e4b0907d9d27f499f59e83aa6a728f07
k-roozbahani <kavehrozbah@gmail.com>
2023-02-06 20:30:42 +0330

    defined cart and cartitems serializers

--- plant_shop/products/api/serializers.py
+++ plant_shop/products/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Category, ProductAttribute, Product
+from ..models import Category, ProductAttribute, Product, Cart, CartItem
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -26,3 +26,35 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'categories', 'name', 'price', 'stock', 'categories', 'attributes', 'image')
+
+
+class CartItemCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'count']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        cart = Cart.objects.get(user=user)
+        instance = CartItem.objects.create(cart=cart, **validated_data)
+        return instance
+
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'total_price', 'discount']
+
+
+class CartItemDetailSerializer(serializers.ModelSerializer):
+    price = serializers.SerializerMethodField()
+    product = ProductSerializer()
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'cart', 'product', 'count', 'price']
+
+    def get_price(self, obj):
+        return obj.product.price * obj.count
