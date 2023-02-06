48278d5202dda6dfa700c8415df6021d9c3c559d
k-roozbahani <kavehrozbah@gmail.com>
2023-02-06 10:07:09 +0330

    add url api-categories

--- plant_shop/products/api/urls.py
+++ plant_shop/products/api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProductApiView
+from .views import ProductApiView, CategoryApiView
 
 router = DefaultRouter()
 router.register(r'product', ProductApiView, 'api-products')
+router.register(r'categories', CategoryApiView, 'api-categories')
 urlpatterns = router.urls
