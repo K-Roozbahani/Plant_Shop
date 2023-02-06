c70a91f0566de29af2bd0640f6a2af79926c577d
k-roozbahani <kavehrozbah@gmail.com>
2023-02-06 20:29:49 +0330

    add cart and cartitem urls

--- plant_shop/products/api/urls.py
+++ plant_shop/products/api/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProductApiView, CategoryApiView
+from .views import ProductApiView, CategoryApiView, CartApiView, CartItemApiView
 
 router = DefaultRouter()
-router.register(r'product', ProductApiView, 'api-products')
+router.register(r'products', ProductApiView, 'api-products')
 router.register(r'categories', CategoryApiView, 'api-categories')
+router.register(r'cart', CartApiView, 'api-cart')
+router.register(r'cart/items', CartItemApiView, 'api-cart-items')
 urlpatterns = router.urls
