f33c450adfd8150afd288cf6409e78c7d836d5c4
k-roozbahani <kavehrozbah@gmail.com>
2023-02-12 19:01:05 +0330

    add chechout_retrive to order view class

--- plant_shop/orders/api/serializers.py
+++ plant_shop/orders/api/serializers.py
@@ -2,57 +2,82 @@ from rest_framework import serializers
 from ..models import Order, OrderItem, DeliveryInformation, Checkout
 
 
-class OrderCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ('id', 'user', 'status', 'tracking_code', 'orders_price', 'payment_type', 'is_paid')
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        instance = Order.Create(user=user, **validated_data)
-        return instance
-
-
-class OrderDetailSerializer(serializers.models):
-    user = serializers.StringRelatedField()
-    status = serializers.SerializerMethodField()
-    payment_type = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Order
-        fields = ('id', 'user', 'status', 'tracking_code', 'orders_price', 'payment_type', 'is_paid')
-
-    def get_status(self, obj):
-        return obj.get_status_display()
-
-    def get_payment_type(self, obj):
-        return obj.get_payment_type()
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        instance = Order.Create(user=user, **validated_data)
-        return instance
-
-
 class OrderItemSerializer(serializers.ModelSerializer):
-    order = OrderDetailSerializer(write_only=True)
     price = serializers.SerializerMethodField()
-
-    # product = ProductSerializer()
+    order = serializers.PrimaryKeyRelatedField(queryset=Order.valid_objects.all(), allow_empty=True)
 
     class Meta:
-        model = OrderItem()
-        fields = ['order', 'product', 'expire_time', 'count', 'price', 'created_time', 'modified_time']
+        model = OrderItem
+        fields = ['order', 'product', 'expire_time', 'count', 'price']
 
     def get_price(self, obj):
         return obj.get_price()
-
-    # def validate_order(self, value):
 
     def validate(self, data):
         if data['count'] > data['product'].stock:
             raise serializers.ValidationError('product is not exist')
         return super(OrderItemSerializer, self).validate(data)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True)
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ('id', 'is_open','user', 'get_status_display', 'orders_price',
+                  'get_payment_type_display', 'is_paid', 'order_items', 'status', 'payment_type')
+        read_only_fields = ('orders_price', 'is_paid')
+        extra_kwargs = {'status': {'write_only': True, 'default': 1},
+                        'payment_type': {'write_only': True, 'default': 1}}
+
+    def create(self, validated_data):
+        order_items_data = validated_data.pop('order_items')
+        user = self.context['request'].user
+        if not order_items_data:
+            return serializers.ValidationError('order_items can not empty')
+        order = Order.objects.create(user=user, **validated_data)
+        for order_item_data in order_items_data:
+            order_item = OrderItem.objects.create(order=order, **order_item_data)
+            order.order_items.add(order_item)
+            order.orders_price += order_item.get_price()
+        order.save()
+        return order
+
+    def update(self, instance, validated_data):
+        items_instance = instance.order_items.all()
+        items_data = validated_data.pop("order_items")
+        price = 0
+        for item_data in items_data:
+            item_id = item_data.get("id")
+            item_count = item_data.get("count")
+            new_items = []
+            edited_items = []
+
+            # If order item is exits update it else create new order itme
+
+            # --------------------------------------------------> edite order item
+            if item_id:
+                try:
+                    item_instance = items_instance.get(id=item_id)
+                except:
+                    raise serializers.ValidationError("Order Item dose not exist")
+                item_instance.count = item_count
+                if item_instance.is_available():
+                    # item_instance.save() ------------------------------->save
+                    edited_items.append(item_instance)
+                    price += item_instance.get_price()
+                else:
+                    raise serializers.ValidationError("This number of product is not available in stock")
+
+            # --------------------------------------------------> crete new order item
+            elif not item_id:
+                item_instance = OrderItem(order=instance, **item_data)
+                if item_instance.is_available():
+                    new_items.append(item_instance)
+                    price += item_instance.get_price()
+                else:
+                    raise serializers.ValidationError("This number of product is not available in stock")
 
 
 class DeliveryInformationSerializer(serializers.ModelSerializer):
@@ -63,8 +88,28 @@ class DeliveryInformationSerializer(serializers.ModelSerializer):
 
 class CheckoutSerializer(serializers.ModelSerializer):
     delivery_information = DeliveryInformationSerializer()
+    order = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Checkout
-        fields = ('id', 'delivery_information', 'order', 'post_type', 'send_cost', 'tracking_code')
+        fields = ('id', 'order', 'delivery_information', 'order', 'post_type', 'send_cost', 'tracking_code')
 
+# class OrderDetailSerializer(serializers.ModelSerializer):
+#     user = serializers.StringRelatedField()
+#     status = serializers.SerializerMethodField()
+#     payment_type = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = Order
+#         fields = ('id', 'user', 'status', 'orders_price', 'payment_type', 'is_paid')
+#
+#     def get_status(self, obj):
+#         return obj.get_status_display()
+#
+#     def get_payment_type(self, obj):
+#         return obj.get_payment_type()
+#
+#     def create(self, validated_data):
+#         user = self.context['request'].user
+#         instance = Order.Create(user=user, **validated_data)
+#         return instance
