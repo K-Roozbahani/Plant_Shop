dc5db6072f7c9d9a0191f021c627e9b90a1d0237
k-roozbahani <kavehrozbah@gmail.com>
2023-02-16 17:49:22 +0330

    change tamplate for products view

--- plant_shop/products/views.py
+++ plant_shop/products/views.py
@@ -6,9 +6,9 @@ def product_view(request, pk=None):
     if not pk:
         api_products = ProductApiView.as_view({'get': 'list'})(request).data
         context = {'products': api_products}
-        return render(request, 'products.html', context)
+        return render(request, 'shop.html', context)
     elif pk:
         if not pk:
             api_product = ProductApiView.as_view({'get': 'retrieve'})(request).data
             context = {'products': api_product}
-            return render(request, 'products.html', context)
+            return render(request, 'shop.html', context)
