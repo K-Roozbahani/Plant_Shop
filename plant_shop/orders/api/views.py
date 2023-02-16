1948cd5e00aa5db8fdd38f975b8bd4a389929e0f
k-roozbahani <kavehrozbah@gmail.com>
2023-02-16 12:35:30 +0330

    add checkout action to OrdersApiView class

--- plant_shop/orders/api/views.py
+++ plant_shop/orders/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .serializers import (OrderDetailSerializer, OrderItemSerializer, OrderSerializer,
+from .serializers import (OrderItemSerializer, OrderSerializer,
                           DeliveryInformationSerializer, CheckoutSerializer)
 from ..models import OrderItem, Order, DeliveryInformation, Checkout
 
@@ -15,3 +17,14 @@ class OrderApiView(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         return Order.valid_objects.prefetch_related('order_items').filter(user=user)
+
+    @action(methods="get", detail=True, url_path='<int:pk>/checkout/', url_name="checkout_retrieve")
+    def checkout_retrieve(self, request, pk=None):
+        try:
+            checkout = Checkout.valid_objects.prefetch_related("delivery_information", "order").get(order__id=pk)
+        except Checkout.DoesNotExist:
+            return Response("order not found", status=status.HTTP_404_NOT_FOUND)
+        if checkout.order.user != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        serializer = CheckoutSerializer(instance=checkout)
+        return Response(serializer.data, status=status.HTTP_200_OK)
