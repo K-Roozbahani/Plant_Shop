ba6e757b6569e4cbad1f1af853ccb16c98a91d65
k-roozbahani <kavehrozbah@gmail.com>
2023-02-19 11:36:08 +0330

    fixed product view

--- plant_shop/products/views.py
+++ plant_shop/products/views.py
@@ -3,13 +3,14 @@ from .api.views import ProductApiView, CartApiView
 
 
 def product_view(request, pk=None):
-    cart = CartApiView.as_view({'get:': 'list'})(request).data
+    cart = CartApiView.as_view({'get': 'list'})(request).data
+    print(cart)
     if not pk:
         api_products = ProductApiView.as_view({'get': 'list'})(request).data
-        context = {'products': api_products, 'cart_items': cart}
+        context = {'products': api_products, 'cart': cart}
         return render(request, 'shop.html', context)
     elif pk:
         if not pk:
             api_product = ProductApiView.as_view({'get': 'retrieve'})(request).data
-            context = {'product': api_product, 'cart_items': cart}
+            context = {'product': api_product, 'cart': cart}
             return render(request, 'shop.html', context)
