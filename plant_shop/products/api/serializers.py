8b4516cfead1185a519667f4d0e39493dbdceebc
k-roozbahani <kavehrozbah@gmail.com>
2023-02-19 10:48:41 +0330

    convert serialziers cart and cart items to one proper serizelir

--- plant_shop/products/api/serializers.py
+++ plant_shop/products/api/serializers.py
@@ -28,10 +28,30 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ('id', 'categories', 'name', 'price', 'stock', 'categories', 'attributes', 'image')
 
 
-class CartItemCreateSerializer(serializers.ModelSerializer):
+# this class get product primary key and display product data serializer
+class ProductRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        serializer = ProductSerializer(value)
+        product = {"id": serializer.data["id"], "name": serializer.data['name'], "price": serializer.data['price']}
+        return product
+
+    def to_internal_value(self, data):
+        if type(data) != int:
+            raise serializers.ValidationError(f"Input must be integer not {type(data)}")
+        try:
+            product_instance = Product.valid_objects.get(id=data)
+        except Product.DoesExist:
+            raise serializers.ValidationError("Products dose not exist")
+        return product_instance
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField()
+    product = ProductRelatedField(queryset=Product.valid_objects.all())
+
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'count']
+        fields = ['id', 'product', 'count', 'total_price']
 
     def create(self, validated_data):
         user = self.context['request'].user
@@ -39,22 +59,26 @@ class CartItemCreateSerializer(serializers.ModelSerializer):
         instance = CartItem.objects.create(cart=cart, **validated_data)
         return instance
 
+    def get_total_price(self, obj):
+        return obj.get_price()
+
+
+class CartItemsRelatedField(serializers.RelatedField):
+    def to_representation(self, value=None):
+        if value is None:
+            return None
+
+        else:
+            return CartItemSerializer(instance=value, many=True).data
+
+    def to_internal_value(self, data):
+        pass
+
 
 class CartSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()
+    user = serializers.StringRelatedField(read_only=True)
+    cart_items = CartItemsRelatedField(read_only=True)
 
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'total_price', 'discount']
-
-
-class CartItemDetailSerializer(serializers.ModelSerializer):
-    price = serializers.SerializerMethodField()
-    product = ProductSerializer()
-
-    class Meta:
-        model = CartItem
-        fields = ['id', 'cart', 'product', 'count', 'price']
-
-    def get_price(self, obj):
-        return obj.product.price * obj.count
+        fields = ['id', 'user', 'total_price', 'discount', 'cart_items']
