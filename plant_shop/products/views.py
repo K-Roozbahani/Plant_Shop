cc26095542be4c08ca9c086dff38b3cba4037085
k-roozbahani <kavehrozbah@gmail.com>
2023-02-25 12:24:22 +0330

    change products template name in products.views

--- plant_shop/products/views.py
+++ plant_shop/products/views.py
@@ -10,9 +10,9 @@ class ProductView(View):
         if not pk:
             api_products = ProductApiView.as_view({'get': 'list'})(request).data
             context = {'products': api_products, 'cart': cart}
-            return render(request, 'shop.html', context)
+            return render(request, 'products_list.html', context)
         elif pk:
             if not pk:
                 api_product = ProductApiView.as_view({'get': 'retrieve'})(request).data
                 context = {'product': api_product, 'cart': cart}
-                return render(request, 'shop.html', context)
+                return render(request, 'products_list.html', context)
