600f0d0f743652568d2243a8882d6a9ca0a56fc0
k-roozbahani <kavehrozbah@gmail.com>
2023-03-22 15:51:29 +0330

    hendel errors code for products responces

--- plant_shop/products/views.py
+++ plant_shop/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.views import View
 
 from .api.views import ProductApiView, CartApiView
@@ -6,12 +6,18 @@ from .api.views import ProductApiView, CartApiView
 
 class ProductView(View):
     def get(self, request, pk=None):
-        cart = CartApiView.as_view({'get': 'list'})(request).data
+        cart = CartApiView.as_view({'get': 'list'})(request)
+        if cart.status_code != 200:
+            HttpResponse(status=404, content="somthing wrong happen")
         if not pk:
-            api_products = ProductApiView.as_view({'get': 'list'})(request).data
-            context = {'products': api_products, 'cart': cart}
+            api_products = ProductApiView.as_view({'get': 'list'})(request)
+            if api_products.status_code != 200:
+                HttpResponse(status=404, content="somthing wrong happen")
+            context = {'products': api_products.data, 'cart': cart.data}
             return render(request, 'shop-fullwidth.html', context)
         elif pk:
-            api_product = ProductApiView.as_view({'get': 'retrieve'})(request=request, pk=pk).data
-            context = {'product': api_product, 'cart': cart}
+            api_product = ProductApiView.as_view({'get': 'retrieve'})(request=request, pk=pk)
+            if api_product.status_code != 200:
+                HttpResponse(status=404, content="product not found")
+            context = {'product': api_product.data, 'cart': cart.data}
             return render(request, 'product-details.html', context)
