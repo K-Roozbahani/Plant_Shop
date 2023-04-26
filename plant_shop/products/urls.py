36e1959578e78cb39fd1332c866d854d211c24a8
k-roozbahani <kavehrozbah@gmail.com>
2023-04-26 07:56:14 +0330

    add cart url into products.urls

--- plant_shop/products/urls.py
+++ plant_shop/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductView
+from .views import ProductView, CartView
 urlpatterns = [
     path('', ProductView.as_view(), name='products_view'),
     path('<int:pk>/', ProductView.as_view(), name='product_detail'),
+    path('cart/', CartView.as_view(), name='cart_items'),
+    path('cart/remove/<int:pk>/', CartView.as_view(), name='cart_item_remove')
 ]
