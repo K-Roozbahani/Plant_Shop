0dbba527761d38781fc63b687b3ce2f870512224
k-roozbahani <kavehrozbah@gmail.com>
2023-04-30 09:18:54 +0330

    chaned cart_items urls name

--- plant_shop/products/urls.py
+++ plant_shop/products/urls.py
@@ -3,6 +3,6 @@ from .views import ProductView, CartView
 urlpatterns = [
     path('', ProductView.as_view(), name='products_view'),
     path('<int:pk>/', ProductView.as_view(), name='product_detail'),
-    path('cart/', CartView.as_view(), name='cart_items'),
+    path('cart/', CartView.as_view(), name='cart'),
     path('cart/remove/<int:pk>/', CartView.as_view(), name='cart_item_remove')
 ]
