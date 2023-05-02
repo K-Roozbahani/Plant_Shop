4944046bc5665fbc11021fa60ecf3720b4635769
k-roozbahani <kavehrozbah@gmail.com>
2023-05-02 07:39:21 +0330

    add DelivaryInformation form in orders.forms

---
+++ plant_shop/orders/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import DeliveryInformation
+
+
+class DeliveryInformationForm(forms.ModelForm):
+    class Meta:
+        model = DeliveryInformation
+        fields = ["first_name", "last_name", "address", "state", "city",
+                  "postal_cod", "email", "phone", "description"]
