678482068ac83724a36feebc2284d8ae3b9a46bf
k-roozbahani <kavehrozbah@gmail.com>
2023-05-05 12:40:27 +0330

    fixed checkout form fields neme

--- plant_shop/orders/forms.py
+++ plant_shop/orders/forms.py
@@ -6,4 +6,4 @@ class DeliveryInformationForm(forms.ModelForm):
     class Meta:
         model = DeliveryInformation
         fields = ["first_name", "last_name", "address", "state", "city",
-                  "postal_cod", "email", "phone", "description"]
+                  "postal_code", "email", "phone_number", "description"]
