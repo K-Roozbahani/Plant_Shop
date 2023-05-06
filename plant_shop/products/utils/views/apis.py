18aecea1f1e0cb510011dead6875ef75239f3f32
k-roozbahani <kavehrozbah@gmail.com>
2023-05-06 13:01:13 +0330

    add get_cart method to products utils

---
+++ plant_shop/products/utils/views/apis.py
@@ -0,0 +1,12 @@
+from django.http import Http404
+
+from ...api.views import CartApiView
+
+
+def get_cart(request):
+    if not request.user:
+        return None
+    cart = CartApiView.as_view({'get': 'list'})(request)
+    if cart.status_code != 200:
+        raise Http404("user need to log")
+    return cart.data
