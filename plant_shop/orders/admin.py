84a82fc3b9e8c4e850c9e3e14ffb559d3729d3ec
k-roozbahani <kavehrozbah@gmail.com>
2023-06-02 17:49:38 +0330

    define orders admin models

--- plant_shop/orders/admin.py
+++ plant_shop/orders/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import DeliveryInformation, Order, Checkout, OrderItem
 
-# Register your models here.
+
+class OrderItemTabularInLine(admin.TabularInline):
+    model = OrderItem
+    fields = ("product", "count", "expire_time")
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["created_time", "updated_time"]
+    inlines = [OrderItemTabularInLine]
+
+
+@admin.register(DeliveryInformation)
+class DeliveryInformationAdmin(admin.ModelAdmin):
+    model = DeliveryInformation
+    readonly_fields = ["created_time", "updated_time"]
+
+
+@admin.register(Checkout)
+class CheckoutAdmin(admin.ModelAdmin):
+    model = Checkout
+    readonly_fields = ["created_time", "updated_time"]
