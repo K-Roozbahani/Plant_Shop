249f5f7e68989c6f5512cbb440d72170cd50f5d2
k-roozbahani <kavehrozbah@gmail.com>
2023-06-04 11:13:41 +0330

    fixed Order view class

--- plant_shop/orders/views.py
+++ plant_shop/orders/views.py
@@ -1,25 +1,31 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.views import View
 from .forms import DeliveryInformationForm
 from .models import DeliveryInformation, Order, OrderItem, Checkout
 from products.models import CartItem, Cart
 from products.utils.views.apis import get_cart
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class CheckoutView(View):
+
+    # @method_decorator(login_required())
     def post(self, request):
+        if not request.user.is_authenticated:
+            return Http404("loging required")
         user = request.user
-        cart = get_cart(request)
         # --check delivery information
         try:
-            delivery_information = DeliveryInformation.valid_objects.get(DeliveryInformation, user=request)
+            delivery_information = DeliveryInformation.valid_objects.get(user=request.user)
         except DeliveryInformation.DoesNotExist:
             delivery_information = DeliveryInformation(user=user)
 
         # --form
-        form = DeliveryInformationForm(data=request.post, instance=delivery_information)
+        form = DeliveryInformationForm(data=request.POST, instance=delivery_information)
         if not form.is_valid():
+            request.method = "get"
             return self.get(request=request, form=form)
         elif form.has_changed():
             form.save()
@@ -30,19 +36,26 @@ class CheckoutView(View):
         order_items = []
         cart_items = CartItem.valid_objects.filter(cart__user=user).select_related("product")
         for item in cart_items:
-            order_item = OrderItem(order=order, product=item.product, conut=item.count)
+            order_item = OrderItem(order=order, product=item.product, count=item.count)
+            item.is_valid = False
             if order_item.is_available():
                 order.orders_price += order_item.get_price()
-        order_items = OrderItem.valid_object.bulk_craete(order_items)
+                order_items.append(order_item)
+        order_items = OrderItem.valid_objects.bulk_create(order_items)
         if order_items:
             order.save()
             Checkout.objects.create(delivery_information=delivery_information, order=order)
-            return render(request, "checkout.html", context={"cart": cart, "form": form})
+            CartItem.objects.bulk_update(cart_items, ["is_valid"])
+        return HttpResponseRedirect("/products/")
 
+    # @method_decorator(login_required())
     def get(self, request, pk=None, form=None):
+        if not request.user.is_authenticated:
+            raise Http404("loging required")
         cart = get_cart(request)
         # display forms errors
         if form:
+            print("in method get")
             return render(request, "checkout.html", context={"cart": cart, "form": form})
         # display empty form to get information
         elif not form and not pk:
@@ -53,7 +66,7 @@ class CheckoutView(View):
                 except DeliveryInformation.DoesNotExist:
                     form = DeliveryInformationForm()
             else:
-                return HttpResponseRedirect("products/cart/")
+                return HttpResponseRedirect("/products/cart/")
             return render(request, "checkout.html", context={"cart": cart, "form": form})
         # display order information
         elif pk:
