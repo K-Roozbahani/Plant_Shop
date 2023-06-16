b6350db55854cdb5ee53aba29b905b5f9f78b685
k-roozbahani <kavehrozbah@gmail.com>
2023-06-16 17:29:20 +0330

    initial definition of UserLogin class

--- plant_shop/users/views.py
+++ plant_shop/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+class LoginUser(View):
+    def get(self, request, failed_form=None):
+        if failed_form is not None:
+            form = AuthenticationForm()
+        else:
+            form = failed_form
+        context = {"from": form}
+        return render(request, "login.html", context=context)
+
+    def post(self, request):
+        form = AuthenticationForm(request=request)
+        if form.is_valid():
+            user = form.get_user()
+            if user:
+                login(request=request, user=user)
+                return HttpResponseRedirect("/products/")
+
+        return self.get(request, failed_form=form)
