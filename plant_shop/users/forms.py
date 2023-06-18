842253814c7b4ef8ae5c2024b27acea551951ae8
k-roozbahani <kavehrozbah@gmail.com>
2023-06-18 18:08:36 +0330

    add UserRegisterForm in users.forms

--- plant_shop/users/forms.py
+++ plant_shop/users/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, password_validation
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 from .models import User
 
+
+# from django.contrib.auth.forms import UserCreationForm
 
 class AuthenticationForm(forms.Form):
     message = "لطفا شماره مبایل صحیح وارد کنید. شماره مبایل را با صفر وارد کنید. نمونه: 09123456789"
@@ -65,3 +67,70 @@ class AuthenticationForm(forms.Form):
             code="invalid_login",
             params={"username": _("phone number")},
         )
+
+
+class UserRegisterForm(forms.ModelForm):
+
+    message = "لطفا شماره مبایل صحیح وارد کنید. شماره مبایل را با صفر وارد کنید. نمونه: 09123456789"
+
+    error_messages = {
+        "password_mismatch": "پسورد اول و دوم همسان نمی باشند",
+    }
+
+    phone_number = forms.CharField(max_length=16,
+                                   validators=[RegexValidator(regex=r'^09\d{9}$', message=message)],
+                                   widget=forms.TextInput(attrs={"placeholder": "شماره مبایل\t 09123456789 "}))
+    first_name = forms.CharField(max_length=150, widget=forms.TextInput(attrs={"placeholder": "نام"}), )
+    last_name = forms.CharField(max_length=150, widget=forms.TextInput(attrs={"placeholder": "نام خانوادگی"}), )
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password", "placeholder": "رمز عبور"}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password", "placeholder": "تکرار رمز عبور"}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
+    class Meta:
+        model = User
+        fields = ("phone_number", "first_name", "last_name")
+
+    def __init__(self, request=None, *args, **kwargs):
+        if request is not None:
+            kwargs["data"] = request.POST
+        super().__init__(*args, **kwargs)
+        self.fields[self._meta.model.USERNAME_FIELD].widget.attrs[
+            "autofocus"
+        ] = True
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+            )
+        return password2
+
+    def _post_clean(self):
+        super()._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
+        password = self.cleaned_data.get("password2")
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except ValidationError as error:
+                self.add_error("password2", error)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
