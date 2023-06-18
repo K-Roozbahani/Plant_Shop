29f8d1fed0cf09cda746464dab27cc7eea46d3ec
k-roozbahani <kavehrozbah@gmail.com>
2023-06-18 18:11:03 +0330

    add UserRegisterView in users.views

--- plant_shop/users/views.py
+++ plant_shop/users/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth import login
 from django.http import HttpResponseRedirect
-from .forms import AuthenticationForm
+from .forms import AuthenticationForm, UserRegisterForm
 
 
-class LoginUser(View):
+class UserLoginView(View):
     def get(self, request, failed_form=None):
 
         if request.user.is_authenticated:
@@ -20,6 +20,27 @@ class LoginUser(View):
         form = AuthenticationForm(request=request)
         if form.is_valid():
             user = form.get_user()
+            if user:
+                login(request=request, user=user)
+                return HttpResponseRedirect("/products/")
+        return self.get(request, failed_form=form)
+
+
+class UserRegisterView(View):
+    def get(self, request, failed_form=None):
+
+        if request.user.is_authenticated:
+            return HttpResponseRedirect("/products/")
+        form = failed_form
+        if form is None:
+            form = UserRegisterForm()
+        context = {"form": form}
+        return render(request, "register.html", context=context)
+
+    def post(self, request):
+        form = UserRegisterForm(request=request)
+        if form.is_valid():
+            user = form.save()
             if user:
                 login(request=request, user=user)
                 return HttpResponseRedirect("/products/")
