c1828028340e105f939ccc0e1cdc66e3630beb3a
k-roozbahani <kavehrozbah@gmail.com>
2023-07-06 02:08:15 +0330

    add profile ulr

--- plant_shop/users/urls.py
+++ plant_shop/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UserLoginView, UserRegisterView
+from .views import UserLoginView, UserRegisterView, UserProfileView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
